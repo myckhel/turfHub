e30245d10627233ee2a0f745ae5bec5139b136ec
myckhel <myckhel1@hotmail.com>
2025-06-10 12:55:45 +0100

    fix: add missing path aliases for easier imports

--- vite.config.ts
+++ vite.config.ts
@@ -20,6 +20,8 @@ export default defineConfig({
     resolve: {
         alias: {
             'ziggy-js': resolve(__dirname, 'vendor/tightenco/ziggy'),
+            '@': resolve(__dirname, 'resources/js'),
+            '@components': resolve(__dirname, 'resources/js/components'),
         },
     },
 });
